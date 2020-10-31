fa271c4b18bdd2cfe1d939d23f976b20adfd2e87
Simon Y <simon.yorkston@gmail.com>
2020-10-31 19:53:13 +0100

    Fighting selectors

--- src/js/form-util.js
+++ src/js/form-util.js
@@ -39,7 +39,7 @@ function fetchLocalStorage(formInputs) {
           field.id === 'field-lastname'   ||
           field.id === 'field-birthday'   ||
           field.id === 'field-placeofbirth') {
-        $('#'.field.id).value = localStorage.getItem(field.id);
+        $(field).value = localStorage.getItem(field.id);
       }
     }
 }
